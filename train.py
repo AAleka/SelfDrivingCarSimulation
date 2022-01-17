38eebca5de934459d9637032e3f3c46b7253ec8f
AAleka <56869115+AAleka@users.noreply.github.com>
2022-01-17 12:47:28 +0300

    Update train.py

--- train.py
+++ train.py
@@ -15,20 +15,16 @@ if gpus:
         # Virtual devices must be set before GPUs have been initialized
         print(e)
 
-#### STEP 1
 path = 'Data'
 data = importDataInfo(path)
 
-#### STEP 2
 data = balanceData(data, display=True)
 
-#### STEP 3
 imagesPath, steerings = loadData(path, data)
 
 del data
 del path
 
-#### STEP 4
 xTrain, xVal, yTrain, yVal = train_test_split(imagesPath, steerings, test_size=0.25, random_state=5)
 print('Total Training Images:', len(xTrain))
 print('Total Validation Images:', len(xVal))
@@ -36,25 +32,13 @@ print('Total Validation Images:', len(xVal))
 del imagesPath
 del steerings
 
-#### STEP 5
-
-
-#### STEP 6
-
-
-#### STEP 7
-
-
-#### STEP 8
 model = createModel()
 model.summary()
 callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True)
 
-#### STEP 9
 history = model.fit(batchGen(xTrain, yTrain, 100, 1), steps_per_epoch=200, epochs=50,
                     validation_data=batchGen(xVal, yVal, 100, 0), validation_steps=200, callbacks=[callback])
 
-#### STEP 10
 model.save('model.h5')
 
 plt.plot(history.history['loss'])
